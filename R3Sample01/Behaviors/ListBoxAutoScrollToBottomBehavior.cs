8f066a57f779af222c425b0b9cc79a6e9f6ac3c9
Naoki Tomita <ntomita@safie.jp>
2025-02-03 16:45:09 +0900

    QueueWork

---
+++ R3Sample01/Behaviors/ListBoxAutoScrollToBottomBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Diagnostics;
+
+namespace R3Sample01.Behaviors
+{
+    class ListBoxAutoScrollToBottomBehavior
+    {
+        public static ScrollViewer? GetScrollToBottomParent(DependencyObject obj)
+        {
+            return obj.GetValue(ScrollToBottomParentProperty) as ScrollViewer;
+        }
+
+        public static void SetScrollToBottomParent(DependencyObject obj, ScrollViewer value)
+        {
+            obj.SetValue(ScrollToBottomParentProperty, value);
+        }
+
+        public static readonly DependencyProperty ScrollToBottomParentProperty =
+            DependencyProperty.RegisterAttached("ScrollToBottomParent",
+                typeof(ScrollViewer),
+                typeof(ListBoxAutoScrollToBottomBehavior),
+                new PropertyMetadata(null, (d, e) =>
+                {
+                    if (d is FrameworkElement fe)
+                    {
+                        fe.SizeChanged += OnSizeChanged;
+                    }
+                }));
+        private static void OnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (sender is FrameworkElement fe)
+            {
+                GetScrollToBottomParent(fe)?.ScrollToBottom();
+            }
+        }
+    }
+}
---
+++ R3Sample01/ViewModels/QueueWorkViewModel.cs
@@ -0,0 +1,37 @@
+﻿using R3;
+using R3Sample01.Common;
+using System.Collections.ObjectModel;
+
+namespace R3Sample01.ViewModels
+{
+    public class QueueWorkViewModel : DisposableBase
+    {
+        public BindableReactiveProperty<string?> Text { get; } = new(string.Empty);
+        public ObservableCollection<string> Results { get; } = new();
+
+        public QueueWorkViewModel()
+        {
+            var items = new List<char>();
+
+            this.Text
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Do(x => items.Add(x![^1]))
+                .Select(_ => false)
+                .Merge(this.Text.Debounce(TimeSpan.FromSeconds(2)).Select(_ => true))
+                .Select(force =>
+                {
+                    if (force || items.Count >= 3)
+                    {
+                        var arr = items.ToArray();
+                        items.Clear();
+                        return arr;
+                    }
+                    return [];
+                })
+                .Where(x => x.Length > 0)
+                .Select(x => new string(x))
+                .Subscribe(this.Results.Add)
+                .AddTo(ref this.disposables);
+        }
+    }
+}
